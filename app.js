1f8e95764ddc344b7712ecdb424fba2ebb10b138
elvin <elvinmonteiro77@gmail.com>
2023-07-10 10:08:30 -0400

    draw on tiles being hovered

--- app.js
+++ app.js
@@ -8,5 +8,14 @@ for (let i  = 0; i <16; i++) {
         const tile = document.createElement('div');
         tile.classList.add('tile');
         rowContainer.append(tile);
+
+        const draw = () => {
+            tile.addEventListener('mouseover', () => {
+                tile.style.backgroundColor = 'black';
+            })
+        } 
+        draw();
     }
+    
 }
+
