b353900a3c0d80f7a76a205c6de206dce45af959
elvin <elvinmonteiro77@gmail.com>
2023-07-10 15:28:02 -0400

    prompt grid size selection

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
-const body = document.querySelector('body');
+const grid = document.querySelector('.grid');
+const sizeSlct = document.querySelector('.size');
+const eraseBtn = document.querySelector('.erase');
 
-for (let i  = 0; i <16; i++) {
-    const rowContainer = document.createElement('div');
-    rowContainer.classList.add('row');
-    body.append(rowContainer);
-    for (let i  = 0; i <16; i++) {
-        const tile = document.createElement('div');
-        tile.classList.add('tile');
-        rowContainer.append(tile);
-
-        const draw = () => {
-            tile.addEventListener('mouseover', () => {
-                tile.style.backgroundColor = 'black';
-            })
-        } 
-        draw();
-    }
+let setGrid = (size = 16) => {
+    for (let i  = 0; i < `${size}`; i++) {
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('row');
+        grid.append(rowContainer);
+        for (let i  = 0; i < `${size}`; i++) {
+            const tile = document.createElement('div');
+            tile.classList.add('tile');
+            rowContainer.append(tile);
     
+            const draw = () => {
+                tile.addEventListener('mouseover', () => {
+                    tile.style.backgroundColor = 'black';
+                })
+            } 
+            draw();
+        }
+    }
 }
+setGrid();
+
+sizeSlct.addEventListener('click', () => {
+    let newSize = parseInt(prompt('select grid size'));
+    if (newSize > 100 || newSize < 2 || !Number.isInteger(newSize)) {
+        alert('Grid size mus be a number between 2 and 100')
+    } else {
+        while (grid.hasChildNodes()) {
+            grid.removeChild(grid.firstChild);
+        }
+        setGrid(newSize);
+    }
+})
+
 
